553e980083883b54f5d251eccd353361de02c60e
lebedev-023046 <lebedev.023046@gmail.com>
2022-01-30 23:55:52 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
-  
+    newString = ''
+    nString = String(n)
+    if (nString.slice(0,1) === "-") {
+        n = Number(nString.slice(1, nString.length))
+        while (n != 0) {
+            number = n % 10;
+            newString += number;
+            n = parseInt(n/10);
+        }
+    }
+    
+    while (n != 0) {
+        number = n % 10;
+        newString += number;
+        n = parseInt(n/10);
+    }
+return Number(newString)
 }
